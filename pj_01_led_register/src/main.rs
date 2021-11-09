938b6c64ea4bf90b15bff3b3b06913f1b3e4878c
iwatake2222 <take.iwiw2222@gmail.com>
2021-11-10 08:00:45 +0900

    [pj_01_led_register] implement

--- pj_01_led_register/src/main.rs
+++ pj_01_led_register/src/main.rs
@@ -8,6 +8,14 @@ use wio::{entry};
 
 #[entry]
 fn main() -> ! {
+
+    unsafe {
+        const PA_DIRSET: u32 = 0x41008008;
+        const PA_OUTSET: u32 = 0x41008018;
+        *(PA_DIRSET as *mut u32) = 1 << 15;
+        *(PA_OUTSET as *mut u32) = 1 << 15;
+    }
+
     loop {
     }
 }
