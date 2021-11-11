b003ea055d73ced602ed4b176cf51aeb4751342a
iwatake2222 <take.iwiw2222@gmail.com>
2021-11-12 08:32:02 +0900

    [pj_03_gpio_by_hal] done

--- pj_03_gpio_by_hal/src/main.rs
+++ pj_03_gpio_by_hal/src/main.rs
@@ -3,8 +3,25 @@
 
 use panic_halt as _;
 use cortex_m_rt::entry;
+use atsamd_hal::{prelude::*, gpio::v2::Pins};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let peripherals = atsamd_hal::pac::Peripherals::take().unwrap();
+    // let peripherals = atsamd_hal::target_device::Peripherals::take().unwrap();   // same
+
+    // let mut pins = peripherals.PORT.split();      // gpio v1 is deprecated
+    let pins = Pins::new(peripherals.PORT);     // so, use gpio v2
+    let mut led = pins.pa15.into_push_pull_output();       // USER_LED = PA15 = Output
+    let button1 = pins.pc26.into_floating_input(); // Button1 = PC26 = Input
+
+    loop {
+        if button1.is_high().unwrap() {
+            /* Button is not pressed */
+            led.set_high().unwrap();
+        } else {
+            /* Button is pressed */
+            led.set_low().unwrap();
+        }
+    }
 }
